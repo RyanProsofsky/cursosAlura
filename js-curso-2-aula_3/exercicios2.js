e74b4ce81c17b1ba8f90702f45ab882d2dd561cb
Ryan Prosofsky <93451770+RyanProsofsky@users.noreply.github.com>
2024-01-24 09:47:57 -0300

    exercicios prontos

---
+++ js-curso-2-aula_3/exercicios2.js
@@ -0,0 +1,22 @@
+//Crie uma lista vazia, com o nome listaGenerica.   
+let listaGenerica = []
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let  linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python']
+console.log(linguagensDeProgramacao)
+
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('TypeScript', 'Java')
+console.log(linguagensDeProgramacao)
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Ryan', 'Zeca', 'Jorge']
+console.log(listaNomes[0])
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaNome = ['Ryan', 'Zeca', 'Jorge']
+console.log(listaNomes[1])
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaNomesTeste = ['Ryan', 'Zeca', 'Jorge']
+console.log(listaNomes[2])
